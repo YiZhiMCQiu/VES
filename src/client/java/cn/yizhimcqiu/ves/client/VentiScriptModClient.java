687b4737e6cb99c3519eb15244578ef85c815119
YiZhiMCQiu <pikachu20130124@163.com>
2024-12-12 21:25:56 +0800

    忘注册了……\n KGitHub天下第一!!!

--- src/client/java/cn/yizhimcqiu/ves/client/VentiScriptModClient.java
+++ src/client/java/cn/yizhimcqiu/ves/client/VentiScriptModClient.java
@@ -1,11 +1,12 @@
 package cn.yizhimcqiu.ves.client;
 
+import cn.yizhimcqiu.ves.client.commands.VESSettingsCommand;
 import net.fabricmc.api.ClientModInitializer;
 
 public class VentiScriptModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        VESSettingsCommand.register();
     }
 }
