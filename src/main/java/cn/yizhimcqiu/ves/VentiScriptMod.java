62da138be372d8741f9bea4971cdba0b3eff3af8
YiZhiMCQiu <pikachu20130124@163.com>
2025-01-31 14:28:40 +0800

    修复第一次使用VES且未创建ves文件夹时发生的崩溃

--- src/main/java/cn/yizhimcqiu/ves/VentiScriptMod.java
+++ src/main/java/cn/yizhimcqiu/ves/VentiScriptMod.java
@@ -21,19 +21,18 @@ public class VentiScriptMod implements ModInitializer {
         this.registerCommands();
         this.updateEnvironment();
 
+        checkVESFolder();
         VEScriptExecutor.initialize();
-
         CustomItemManager.initialize();
     }
 
     private void updateEnvironment() {
         try {
-            Class.forName("net.minecraft.class_2561");
-        } catch (ClassNotFoundException e) {
+            Class.forName("net.minecraft.entity.mob.CreeperEntity");
             isDevelop = true;
             LOGGER.info("Development environment detected");
             updateVES();
-        }
+        } catch (ClassNotFoundException ignored) { }
     }
 
     private void registerCommands() {
@@ -65,5 +64,15 @@ public class VentiScriptMod implements ModInitializer {
                 return FileVisitResult.CONTINUE;
             }
         });
+    }
+    private void checkVESFolder() {
+        Path path = Path.of("ves");
+        try {
+            if (Files.exists(path)) {
+                Files.createDirectory(path);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
