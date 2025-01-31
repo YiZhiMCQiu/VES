af57dcad4f8d8534ccc8980be7f3a695316b0dd8
YiZhiMCQiu <pikachu20130124@163.com>
2025-01-31 20:06:36 +0800

    添加Gitee仓库

--- src/main/java/cn/yizhimcqiu/vine/installer/VineBuiltinInstaller.java
+++ src/main/java/cn/yizhimcqiu/vine/installer/VineBuiltinInstaller.java
@@ -14,23 +14,25 @@ import java.util.zip.ZipInputStream;
  * VES安装器
  */
 public class VineBuiltinInstaller {
-    private static final String RESOURCE_PATH = "https://yizhimcqiu.github.io/resources/ves_builtin.zip";
+    private static final String GITHUB_PAGES_RESOURCE = "https://yizhimcqiu.github.io/resources/ves_builtin.zip";
+    private static final String GITEE_REPOSITORY_RESOURCE = "https://gitee.com/yizhimcqiu/ves-resources/raw/master/ves-builtin.zip";
     private static final String ZIP_SAVE_PATH = "ves_builtin.zip";
-    private static final String VES_FOLDER = "ves";
-    private static void download() throws IOException {
-        URL url = new URL(RESOURCE_PATH);
+    private static final String EXTRACT_PATH = ".";
+    @SuppressWarnings("deprecation")
+    private static void download(String resourcePath) throws IOException {
+        URL url = new URL(resourcePath);
         try (ReadableByteChannel rbc = Channels.newChannel(url.openStream());
              FileOutputStream fos = new FileOutputStream(ZIP_SAVE_PATH)) {
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
         }
     }
     private static void unzip() throws IOException {
-        File file = new File(VES_FOLDER);
+        File file = new File(EXTRACT_PATH);
         if (file.exists() || file.mkdir());
         try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(ZIP_SAVE_PATH))) {
             ZipEntry entry = zipIn.getNextEntry();
             while (entry != null) {
-                String filePath = VES_FOLDER + File.separator + entry.getName();
+                String filePath = EXTRACT_PATH + File.separator + entry.getName();
                 if (!entry.isDirectory()) {
                     extractFile(zipIn, filePath);
                 } else {
@@ -57,11 +59,20 @@ public class VineBuiltinInstaller {
     }
     public static void install() throws IOException {
         try {
-            download();
+            download(GITEE_REPOSITORY_RESOURCE);
             unzip();
             clean();
-        } catch (IOException e) {
-            throw new CrashException(new CrashReport("Error while installing VES!", e));
+        } catch (IOException e1) {
+            /*
+             * 不会有人访问Gitee还会出问题吧……
+             */
+            try {
+                download(GITHUB_PAGES_RESOURCE);
+                unzip();
+                clean();
+            } catch (IOException e2) {
+                throw new CrashException(new CrashReport("Error while installing VES!", e2));
+            }
         }
     }
 }
